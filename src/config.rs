a6580ac794023185bb5a6a234bc43754018f2c3d
xiaopengli89 <x.friday@outlook.com>
2020-06-13 00:16:24 +0800

    reformat code

--- src/config.rs
+++ src/config.rs
@@ -3,19 +3,19 @@ use serde::Deserialize;
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Config {
-	pub storage_class_name: String,
-	pub job_namespace: String,
-	pub job_name: String,
-	pub job_image: String,
-	pub nodes: Vec<Node>,
+    pub storage_class_name: String,
+    pub job_namespace: String,
+    pub job_name: String,
+    pub job_image: String,
+    pub nodes: Vec<Node>,
     pub count: usize,
-	pub capacity: String,
+    pub capacity: String,
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Node {
-	pub host: Option<String>,
+    pub host: Option<String>,
     pub selector: Option<String>,
-	pub root_path: String,
+    pub root_path: String,
 }
--- src/pv.rs
+++ src/pv.rs
@@ -13,126 +13,126 @@ use std::time::Duration;
 use crate::node::NodePv;
 
 pub struct PvManager<'a> {
-	kube_cli: Option<Client>,
-	cfg: &'a Config,
-	names: HashSet<String>,
-	rng: ThreadRng,
-	node_pvs: Option<Vec<NodePv>>,
-	last_job: Option<Pod>,
+    kube_cli: Option<Client>,
+    cfg: &'a Config,
+    names: HashSet<String>,
+    rng: ThreadRng,
+    node_pvs: Option<Vec<NodePv>>,
+    last_job: Option<Pod>,
 }
 
-impl <'a> PvManager<'a> {
-	pub fn new(kube_client: Client, cfg: &'a Config, node_pvs: Vec<NodePv>) -> Self {
-		Self {
-			kube_cli: Some(kube_client),
-			cfg,
-			names: HashSet::new(),
-			rng: thread_rng(),
+impl<'a> PvManager<'a> {
+    pub fn new(kube_client: Client, cfg: &'a Config, node_pvs: Vec<NodePv>) -> Self {
+        Self {
+            kube_cli: Some(kube_client),
+            cfg,
+            names: HashSet::new(),
+            rng: thread_rng(),
             node_pvs: Some(node_pvs),
-			last_job: None,
-		}
-	}
+            last_job: None,
+        }
+    }
 
-	pub async fn current_pvs(&mut self) -> Result<()> {
-		let lp = ListParams::default();
-		// take client
-		let pvs: Api<PersistentVolume> = Api::all(self.kube_cli.take().unwrap());
-		let pv_list = pvs.list(&lp).await?;
-		// put back client
+    pub async fn current_pvs(&mut self) -> Result<()> {
+        let lp = ListParams::default();
+        // take client
+        let pvs: Api<PersistentVolume> = Api::all(self.kube_cli.take().unwrap());
+        let pv_list = pvs.list(&lp).await?;
+        // put back client
         self.kube_cli = Some(pvs.into_client());
         self.names = pv_list.iter().map(|p| p.name()).collect();
 
-		Ok(())
-	}
+        Ok(())
+    }
 
-	pub async fn create_pvs(&mut self) -> Result<()> {
-		let node_pvs = self.node_pvs.take().unwrap();
-		for n in &node_pvs {
-			let mut volumes = vec![];
-			let mut volume_mounts = vec![];
-			let mut commands = vec![];
-			let mut pv_paths = vec![];
+    pub async fn create_pvs(&mut self) -> Result<()> {
+        let node_pvs = self.node_pvs.take().unwrap();
+        for n in &node_pvs {
+            let mut volumes = vec![];
+            let mut volume_mounts = vec![];
+            let mut commands = vec![];
+            let mut pv_paths = vec![];
 
-			for (i, p) in n.root_paths.iter().enumerate() {
-				let volume_name = format!("data-{}", i);
-				let mount_path = format!("/data/{}", i);
+            for (i, p) in n.root_paths.iter().enumerate() {
+                let volume_name = format!("data-{}", i);
+                let mount_path = format!("/data/{}", i);
 
-				// add volume
-				volumes.push(Volume {
-					name: volume_name.clone(),
-					host_path: Some(HostPathVolumeSource {
-						path: p.0.clone(),
-						..Default::default()
-					}),
-					..Default::default()
-				});
+                // add volume
+                volumes.push(Volume {
+                    name: volume_name.clone(),
+                    host_path: Some(HostPathVolumeSource {
+                        path: p.0.clone(),
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                });
 
-				// add mount
-				volume_mounts.push(VolumeMount {
-					name: volume_name,
-					mount_path: mount_path.clone(),
-					..Default::default()
-				});
+                // add mount
+                volume_mounts.push(VolumeMount {
+                    name: volume_name,
+                    mount_path: mount_path.clone(),
+                    ..Default::default()
+                });
 
-				let pv_names = self.prepare_n_pv(p.1)?;
+                let pv_names = self.prepare_n_pv(p.1)?;
 
-				for name in pv_names {
-					commands.push(format!("mkdir -m 0777 -p {}/{}", mount_path, name));
-					pv_paths.push(PvPath {
-						path: p.0.clone() + "/" + &name,
-						name,
-					});
-				}
-			}
+                for name in pv_names {
+                    commands.push(format!("mkdir -m 0777 -p {}/{}", mount_path, name));
+                    pv_paths.push(PvPath {
+                        path: p.0.clone() + "/" + &name,
+                        name,
+                    });
+                }
+            }
 
-			// mkdir
-			self.job_mkdir(&n.host, volumes, volume_mounts, commands.as_ref()).await?;
+            // mkdir
+            self.job_mkdir(&n.host, volumes, volume_mounts, commands.as_ref()).await?;
 
-			// take client
-			let pvs: Api<PersistentVolume> = Api::all(self.kube_cli.take().unwrap());
-			// create pv
-			for p in pv_paths {
-				let pv = self.get_pv(&p.name, &p.path, &n.host)?;
-				let _ = pvs.create(&PostParams::default(), &pv).await?;
-				println!("PersistentVolume [{}] created", p.name);
-			}
-			// put back client
-			self.kube_cli = Some(pvs.into_client());
-		}
-		self.node_pvs = Some(node_pvs);
-		Ok(())
-	}
+            // take client
+            let pvs: Api<PersistentVolume> = Api::all(self.kube_cli.take().unwrap());
+            // create pv
+            for p in pv_paths {
+                let pv = self.get_pv(&p.name, &p.path, &n.host)?;
+                let _ = pvs.create(&PostParams::default(), &pv).await?;
+                println!("PersistentVolume [{}] created", p.name);
+            }
+            // put back client
+            self.kube_cli = Some(pvs.into_client());
+        }
+        self.node_pvs = Some(node_pvs);
+        Ok(())
+    }
 
-	fn prepare_n_pv(&mut self, count: usize) -> Result<Vec<String>> {
+    fn prepare_n_pv(&mut self, count: usize) -> Result<Vec<String>> {
         let mut pv_names = vec![];
 
-		for _ in 0..count {
-			loop {
-				let name = self.rand_name();
-				if self.names.contains(&name) {
-					continue;
-				}
-				self.names.insert(name.clone());
-				pv_names.push(name);
-				break;
-			}
-		}
+        for _ in 0..count {
+            loop {
+                let name = self.rand_name();
+                if self.names.contains(&name) {
+                    continue;
+                }
+                self.names.insert(name.clone());
+                pv_names.push(name);
+                break;
+            }
+        }
 
-		Ok(pv_names)
-	}
+        Ok(pv_names)
+    }
 
-	fn rand_name(&mut self) -> String {
-		let chars: String = iter::repeat(())
-			.map(|()| self.rng.sample(Alphanumeric))
-			.take(8)
-			.collect();
-		let name = self.cfg.storage_class_name.clone() + "-" + &chars;
-		name.to_lowercase()
-	}
+    fn rand_name(&mut self) -> String {
+        let chars: String = iter::repeat(())
+            .map(|()| self.rng.sample(Alphanumeric))
+            .take(8)
+            .collect();
+        let name = self.cfg.storage_class_name.clone() + "-" + &chars;
+        name.to_lowercase()
+    }
 
-	fn get_pv(&self, name: &str, path: &str, host: &str) -> Result<PersistentVolume> {
-		let pv: PersistentVolume = serde_yaml::from_str(format!(
-			r#"apiVersion: v1
+    fn get_pv(&self, name: &str, path: &str, host: &str) -> Result<PersistentVolume> {
+        let pv: PersistentVolume = serde_yaml::from_str(format!(
+            r#"apiVersion: v1
 kind: PersistentVolume
 metadata:
   name: {name}
@@ -156,21 +156,21 @@ spec:
           - {host}
 "#,
             name = name,
-			capacity = self.cfg.capacity,
-			storage_class_name = self.cfg.storage_class_name,
-			path = path,
-			host = host,
-		).as_ref())?;
+            capacity = self.cfg.capacity,
+            storage_class_name = self.cfg.storage_class_name,
+            path = path,
+            host = host,
+        ).as_ref())?;
         Ok(pv)
-	}
+    }
 
     async fn job_mkdir(&mut self, host: &str, volumes: Vec<Volume>, volume_mounts: Vec<VolumeMount>, commands: &[String]) -> Result<()> {
-		// take client
-		let jobs: Api<Pod> = Api::namespaced(self.kube_cli.take().unwrap(), &self.cfg.job_namespace);
+        // take client
+        let jobs: Api<Pod> = Api::namespaced(self.kube_cli.take().unwrap(), &self.cfg.job_namespace);
 
-		// define job
-		let mut job: Pod = serde_yaml::from_str(format!(
-			r#"apiVersion: v1
+        // define job
+        let mut job: Pod = serde_yaml::from_str(format!(
+            r#"apiVersion: v1
 kind: Pod
 metadata:
   name: {name}
@@ -184,60 +184,59 @@ spec:
     args: ["{commands}"]
   restartPolicy: Never
 "#,
-			name = self.cfg.job_name,
-			host = host,
-			image = self.cfg.job_image,
-			commands = commands.join(" && "),
-		).as_ref())?;
+            name = self.cfg.job_name,
+            host = host,
+            image = self.cfg.job_image,
+            commands = commands.join(" && "),
+        ).as_ref())?;
 
-		let template_spec = job.spec.as_mut().unwrap();
+        let template_spec = job.spec.as_mut().unwrap();
 
-		// define volumes
-		template_spec.volumes = Some(volumes);
+        // define volumes
+        template_spec.volumes = Some(volumes);
 
-		// mount volumes
+        // mount volumes
         template_spec.containers[0].volume_mounts = Some(volume_mounts);
 
         // run job
-     	if self.last_job.is_some() {
+        if self.last_job.is_some() {
             jobs.delete(&self.cfg.job_name, &DeleteParams::default()).await?;
-		} else {
-			if jobs.get(&self.cfg.job_name).await.is_ok() {
-				jobs.delete(&self.cfg.job_name, &DeleteParams::default()).await?;
-			}
-		};
-		let mut o_job = jobs.create(&PostParams::default(), &job).await?;
+        } else {
+            if jobs.get(&self.cfg.job_name).await.is_ok() {
+                jobs.delete(&self.cfg.job_name, &DeleteParams::default()).await?;
+            }
+        };
+        let mut o_job = jobs.create(&PostParams::default(), &job).await?;
 
-		// wait for job complete
-		let mut completed = false;
-		for _ in 0..8 {
-			o_job = jobs.get(&self.cfg.job_name).await?;
+        // wait for job complete
+        let mut completed = false;
+        for _ in 0..8 {
+            o_job = jobs.get(&self.cfg.job_name).await?;
             let status = o_job.status.as_ref().unwrap().phase.as_ref().unwrap();
 
-			if status == "Succeeded" {
-				completed = true;
-				break
-			}
-			if status != "Pending" && status != "Running" {
-				return Err(anyhow::anyhow!("job failed"));
-			}
-			Timer::after(Duration::from_secs(1)).await;
-		}
-		if !completed {
-			return Err(anyhow::anyhow!("job timeout"));
-		}
+            if status == "Succeeded" {
+                completed = true;
+                break;
+            }
+            if status != "Pending" && status != "Running" {
+                return Err(anyhow::anyhow!("job failed"));
+            }
+            Timer::after(Duration::from_secs(1)).await;
+        }
+        if !completed {
+            return Err(anyhow::anyhow!("job timeout"));
+        }
 
-		self.last_job = Some(o_job);
+        self.last_job = Some(o_job);
 
-		// put back client
-		self.kube_cli = Some(jobs.into_client());
+        // put back client
+        self.kube_cli = Some(jobs.into_client());
 
-		Ok(())
-	}
-
+        Ok(())
+    }
 }
 
 struct PvPath {
-	name: String,
-	path: String,
+    name: String,
+    path: String,
 }
